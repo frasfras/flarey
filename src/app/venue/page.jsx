3e40062c69e8b39170cd36a257d9efa04b6fffd5
frasfras <65541080+frasfras@users.noreply.github.com>
2023-10-31 19:00:20 +0800

    Update page.jsx

--- src/app/venue/page.jsx
+++ src/app/venue/page.jsx
@@ -1,29 +1,38 @@
-import Sidebar from './Sidebar'
-import Navbar from './Navbar'
-import VenueInfo from './VenueInfo'
-import PubSubComponent from './PubSubComponent';
+import Sidebar from "../../components/Sidebar";
+import Navbar from "../../components/Navbar";
+import VenueInfo from "../../components/VenueInfo";
+import Chat from "../../components/Chat";
 
-import * as Ably from 'ably';
-import { AblyProvider, useChannel, usePresence } from 'ably/react';
+import * as Ably from "ably";
+import { AblyProvider, useChannel, usePresence } from "ably/react";
 
 export default function Venue() {
   return (
     <>
-    <div className="flex">
-    <Sidebar/>
-    <main className="flex-grow ml-64 relative">
-         <div>
-          <h1>FIND CONCERTS </h1>
-          <p>This is the  Concert Venue details.</p>
-        
-        </div>
-       
-          {/* <Navbar /> */}
-          <VenueInfo />
-          <PubSubComponent />
-          
-    </main>
-    </div>
+      <div className="flex">
+        {/* <Sidebar /> */}
+        <main className="flex-grow ml-64 relative">
+          <div>
+            <h1 style={{ color: "green" }}>FIND CONCERTS </h1>
+            <p>This is the Concert Venue details.</p>
+            <a href="/venue" target="_blank" rel="noopener noreferrer">
+              Share this venue on new tab
+            </a>
+          </div>
+
+          <section className="flex my-4 px-4 gap-2">
+            <div className=" w-1/3 h-[250px] bg-white-700 rounded">
+              <VenueInfo />
+            </div>
+            <div className=" w-1/3 h-[250px] bg-white-700 rounded">
+              {" "}
+              <Chat />
+            </div>
+            <div className=" w-1/3 h-[250px] bg-white-700 rounded"></div>
+          </section>
+        </main>
+      </div>
     </>
-  )
+  );
 }
+
