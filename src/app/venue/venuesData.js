4bd7130365dcf0bcf4304a1f95dab7c55c610727
frasfras <65541080+frasfras@users.noreply.github.com>
2023-11-01 01:13:37 +0800

    Update venuesData.js

--- src/app/venue/venuesData.js
+++ src/app/venue/venuesData.js
@@ -1,33 +1,32 @@
 // venuesData.js
 const venuesData = [
-    {
-      name: 'Venue A',
-      details: 'Venue A details go here.',
-      image: 'https://media.giphy.com/media/7lz6nPd56aHh6/giphy.gif',
-    },
-    {
-      name: 'Venue B',
-      details: 'Venue at West LA.',
-      image: 'https://media.giphy.com/media/q6gzszRDonw3u/giphy.gif',
-    },
-    {
-        name: 'The Forum',
-        details: 'Venue at BGC.',
-        image: 'https://media.giphy.com/media/2AX09OcYVEsUM/giphy.gif',
-    },
-    {
-      name: 'Venue R',
-      details: 'Venue at West Ayala',
-      image: 'https://media.giphy.com/media/DQbNvkxsQPgDR9vRGw/giphy.gif',
-    },
-    {
-      name: 'Venue Rock',
-      details: 'Venue at Magnolia mall.',
-      image: 'https://media.giphy.com/media/26BRKmqUonCPPit1e/giphy.gif',
-    },
+  {
+    name: "Capital Arena",
+    details: "The Celebration Tour.",
+    image: "https://media.giphy.com/media/DQbNvkxsQPgDR9vRGw/giphy.gif",
+  },
+  {
+    name: "Barclays Center",
+    details: "In the Air Tour",
+    image: "https://media.giphy.com/media/q6gzszRDonw3u/giphy.gif",
+  },
+  {
+    name: "The Forum",
+    details: "I Told Them Tour",
+    image: "https://media.giphy.com/media/2AX09OcYVEsUM/giphy.gif",
+  },
+  {
+    name: "United Center",
+    details: "The Scarlet Tour",
+    image: "https://media.giphy.com/media/1qdiF5QNrp4vGQGkRs/giphy.gif",
+  },
+  {
+    name: "Rock Arena",
+    details: "Concert at Magnolia mall.",
+    image: "https://media.giphy.com/media/FEXRDQ9d0kVptw0QCW/giphy.gif",
+  },
 
-    // Add more venues as needed
-  ];
-  
-  export default venuesData;
-  
+  // Add more venues as needed
+];
+
+export default venuesData;
